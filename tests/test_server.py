f955e1e17b85642990d74b6785d88eb4c7955585
Orphée <letembeorphee@gmail.com>
2022-04-23 19:55:37 +0200

    add base html

--- tests/test_server.py
+++ tests/test_server.py
@@ -8,6 +8,7 @@ def test_index_page_loads(client):
     data = response.data.decode()
     assert response.status_code == 200
     assert '<h1>Welcome to the GUDLFT Registration Portal!</h1>' in data
+    assert '<a href="/clubsPoints">Display clubs list</a>' in data
 
 
 # Ensure that club point page loads correctly
@@ -15,5 +16,6 @@ def test_display_clubs_points_page(client):
     response = client.get('/clubsPoints')
     data = response.data.decode()
     assert response.status_code == 200
-    assert "<title>Summary | GUDLFT Clubs points</title>" in data
+    assert "<title>Summary | GUDLFT Clubs</title>" in data
+    assert '<a href="/">Registration</a>' in data
 
